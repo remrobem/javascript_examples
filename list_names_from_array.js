0700af9152ed50bacf526d8937a58848605a272e
remrobem <rob.martin.5758@gmail.com>
2019-02-13 12:59:47 -0500

    lit_names_from_array

---
+++ list_names_from_array.js
@@ -0,0 +1,15 @@
+// Format a string of names like 'Bart, Lisa & Maggie'
+// from an array of objects
+function list(names) {
+    let list = names.map((firstName) => firstName.name);
+    console.log(list)
+    if (names.length > 1) {
+        let last = list.pop();
+        return list.join(', ').concat(' & ', last);
+    } else {
+        return list.toString();
+    }
+}
+
+let test = [ {name: 'Bart'}, {name: 'Lisa'}, {name: 'Marge'} ];
+console.log(list(test));
