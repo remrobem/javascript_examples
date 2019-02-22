32bef9288cdc43ea96ca929b54b3b3b36f2f1c21
remrobem <rob.martin.5758@gmail.com>
2019-02-22 11:47:55 -0500

    updates to find_odd

--- find_odd.js
+++ find_odd.js
@@ -16,23 +16,36 @@
 //   });
 
 function findOdd(A) {
-    // sort the array
-    A.sort();
+
     // initialize object used to get count for each value in array
     var count = {};
+
     // create object with value:count pairs
+    //      count[number] is the key ( the array value)
+    //      count[number] =  sets the value(the count)
     A.forEach(number => {
         count[number] = (count[number] || 0) + 1;
     });
-
+ 
     // loop over object returning first value that is odd
-    for (let key of Object.keys(count)) {
-        if (count[key] % 2 != 0) { return parseInt(key) };
+    for (let number of Object.keys(count)) {
+        if (count[number] % 2 != 0) { return number };
     }
     return undefined;
 }
 
+// bitwise XOR - the values that occur an even number of times cancel themselves out. 
+// 5 ^ 5 = 0
+// the remaining odd value is the response 
 const findOdd1 = (xs) => xs.reduce((a, b) => a ^ b);
 
-console.log(findOdd([20, 5, -1, 2, -2, 3, 3, 5, 1, 2, 4, 20, 4, -1, -2, 1]))
-console.log(findOdd1([20, 3,3,5, 5, 20, 5]))
+// this has to filter every entry in the array
+function findOdd2(arr) {
+    let count = {};
+    arr.map(num => count[num] ? count[num] += 1 : count[num] = 1);
+    return (Object.keys(count).filter(num => count[num] % 2 !== 0).join(''));
+}
+
+console.log(findOdd([20, 5, -1, 2, -2, 3, 3, 5, 1, 2, 4, 20, 4, -1, -2, 1, 1, 5]));
+console.log(findOdd1([20, 3, 3, 5, 5, 20, 5]));
+console.log(findOdd2([20, 5, -1, 2, -2, 3, 3, 5, 5, 1, 2, 4, 20, 4, -1, -2, 1, 1]));
