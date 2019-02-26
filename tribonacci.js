bae9c07e118fa06c23f4ebf0c9030cd6f6251a14
Rob Martin <rob.martin.5758@gmail.com>
2019-02-26 14:28:45 -0500

    tribonacci.js

--- tribonacci.js
+++ tribonacci.js
@@ -28,7 +28,9 @@
 //   Test.assertSimilar(tribonacci([0.5,0.5,0.5],30),[0.5,0.5,0.5,1.5,2.5,4.5,8.5,15.5,28.5,52.5,96.5,177.5,326.5,600.5,1104.5,2031.5,3736.5,6872.5,12640.5,23249.5,42762.5,78652.5,144664.5,266079.5,489396.5,900140.5,1655616.5,3045153.5,5600910.5,10301680.5])
 //   })
 function tribonacci(signature, n) {
-
+if (n < 3) {
+  signature = signature.slice(0,n);
+}
   if (signature.length >= n) {
     return signature
   }
@@ -37,4 +39,6 @@ function tribonacci(signature, n) {
 
 };
 
-console.log((tribonacci([1, 1, 1], 10)))
+console.log(tribonacci([1, 1, 1], 10));
+console.log(tribonacci([1,1,1],1));
+console.log(tribonacci([100,200,300],0));
