7be9af1296c8dcb0d66d0f73b3d1e75e314f6b59
Rob Martin <rob.martin.5758@gmail.com>
2019-03-02 11:25:51 -0500

    cakes.js

--- cakes.js
+++ cakes.js
@@ -17,12 +17,12 @@
 
 // describe('description example', function() {
 //     var recipe, available;
-  
+
 //     it('pass example tests', function() {
 //       recipe = {flour: 500, sugar: 200, eggs: 1};
 //       available = {flour: 1200, sugar: 1200, eggs: 5, milk: 200};
 //       Test.assertEquals(cakes(recipe, available), 2, 'Wrong result for example #1');
-      
+
 //       recipe = {apples: 3, flour: 300, sugar: 150, milk: 100, oil: 100};
 //       available = {sugar: 500, flour: 2000, milk: 2000};
 //       Test.assertEquals(cakes(recipe, available), 0, 'Wrong result for example #2');
@@ -32,21 +32,27 @@
 
 function cakes(recipe, available) {
 
-    const recipeEntries = Object.entries(recipe);
-    // const availableEntries = Object.entries(available);
+
     var quantity = 99999;
 
-    for (const [recipeIngredient, recipeAmount] of recipeEntries) {
-        // console.log(`${recipeIngredient} ${recipeAmount}`)
-        // console.log(available[recipeIngredient])
- 
-        quantity = quantity = Math.floor(available[recipeIngredient]  / recipeAmount ) < quantity ? Math.floor(available[recipeIngredient] / recipeAmount) : quantity;
-        // console.log(Math.floor(available[ingredient] / amount)) 
-        // console.log(quantity)
-      }
-      
-return quantity;
+    for (const recipeIngredient in recipe) {
+        if (!available[recipeIngredient] || available[recipeIngredient] < recipe[recipeIngredient]) {
+            return 0
+        }
+        let ingredientAvailable = Math.floor(available[recipeIngredient] / recipe[recipeIngredient]);
+        quantity = ingredientAvailable < quantity ? ingredientAvailable : quantity;
+    }
+    return quantity;
 
+}
+
+function cakes1(recipe, available) {
+    return Object.keys(recipe).reduce(function(val, ingredient) {
+      return Math.min(Math.floor(available[ingredient] / recipe[ingredient] || 0), val)
+    }, Infinity)  
   }
 
-  console.log(cakes({flour: 500, sugar: 200, eggs: 1}, {flour: 1200, sugar: 1200, eggs: 5, milk: 200}))
+console.log(cakes({ flour: 500, sugar: 200, eggs: 1 }, { flour: 1200, sugar: 1200, eggs: 5, milk: 200 }))
+console.log(cakes({ apples: 3, flour: 300, sugar: 150, milk: 100, oil: 100 }, { sugar: 500, flour: 2000, milk: 2000 }));
+console.log(cakes1({ flour: 500, sugar: 200, eggs: 1 }, { flour: 1200, sugar: 1200, eggs: 5, milk: 200 }))
+console.log(cakes1({ apples: 3, flour: 300, sugar: 150, milk: 100, oil: 100 }, { sugar: 500, flour: 2000, milk: 2000 }));
