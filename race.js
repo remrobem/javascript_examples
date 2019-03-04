5482f4e3a689eb3523b289714c11db6a6e5c9d64
Rob Martin <rob.martin.5758@gmail.com>
2019-03-03 23:29:43 -0500

    race.js updates - not working yet

--- race.js
+++ race.js
@@ -50,6 +50,11 @@ function race1(v1, v2, g) {
     ]
  }
 
+ function race(v1, v2, g){
+    let time=g/(v2-v1);
+    return v2>v1 ? [Math.floor(time),Math.floor(time*60%60),Math.floor(time*3600%60)] : null;
+  }
+
 console.log(race(920, 850, 70));
 console.log(race(80, 91, 37));
 console.log(race(80, 100, 40));
