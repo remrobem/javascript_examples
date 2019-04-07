06ac22b32eed6f39e0ed087f26bb1a7cdee5b3ae
Rob Martin <rob.martin.5758@gmail.com>
2019-04-07 00:15:27 -0400

    changes

---
+++ closure.js
@@ -0,0 +1,11 @@
+var test = (function() {
+    let counter = 0;
+    return function() {
+        counter += 1;
+        return counter;
+    }
+})();
+
+console.log(test())
+console.log(test())
+console.log(test())
