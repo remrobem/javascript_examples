dca9c305b9bc351edadaae689826ca7efbac4dd6
Rob Martin <rob.martin.5758@gmail.com>
2019-05-16 18:19:29 -0400

    variable_reference changes

--- variable_reference.js
+++ variable_reference.js
@@ -2,7 +2,7 @@
 let t1 = 'a';
 let t2 = t1;
 t1 = 'b';
-console.log(`t2: ${t2}`)
+console.log(`t2: ${t2}`) // a
 
 // non-primitive variables copied by reference
 
@@ -12,9 +12,33 @@ let tree = {type: treeType};
 treeType = {value: 'pine'};
 console.log(tree); // oak
 
-// object reference does occur when objext property changed
+// object reference does occur when object property changed
 let carMake = {value: 'Ford'};
 let myCar = {make: carMake};
 carMake.value = 'Toyota';
 console.log(myCar); // Toyota
+
+// array reference: when push or add to original or copied array, both change
+let carModels = ['Prius', 'RAV4'];
+let carList = carModels;
+console.log(`carList: ${carList}`);
+carModels.push('Camry');
+carList.push('F150');
+carModels[4] = 'Focus';
+carList[4] = 'Tacoma';
+console.log(`carList: ${carList}`);
+console.log(`carModels: ${carModels}`);
+
+let ingredients = ['cheese', 'noodles', 'pepper'];
+let ingredientList = ingredients;
+console.log(`ingredients: ${ingredients}`);
+console.log(`ingredientList: ${ingredientList}`);
+ingredients = ['apples', 'flour'];
+ingredients[0] = 'Peaches';
+console.log(`ingredients: ${ingredients}`);
+console.log(`ingredientList: ${ingredientList}`);
+ingredientList = ['lemon', 'water'];
+ingredientList[0] = 'Bananas';
+
+
 
