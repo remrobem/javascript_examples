d3ef3cf50fbaf7d0a6a961b01b8a2c867c1f38fd
Rob Martin <rob.martin.5758@gmail.com>
2019-06-13 13:23:13 -0400

    some spread.js updates

--- spread.js
+++ spread.js
@@ -1,9 +1,33 @@
 // spread expands iterables into one or more arguments
 
 let trees = ['oak', 'hickory'];
-console.log([...trees, 'pine']); // returns all 3 trees
+console.log(`typeof trees: ${typeof trees} isArray: ${Array.isArray(trees)}`);
+console.log([...trees, 'pine']); // returns all 3 trees as array
 
-let wallPaint = {
+
+// Each individual member of the array was plucked from its location and kept inside the receiver. 
+// The receiver, in this case, is a function. 
+// So the individual items become function arguments.
+let text = 'Then there was only one';
+let spreadText = [...text];
+console.log(`typeof spreadText: ${typeof spreadText} isArray: ${Array.isArray(spreadText)}`);
+// Each individual member of the array was plucked from its location and kept inside the receiver. 
+// The receiver, in this case, is a function. 
+// So the individual items become function arguments.
+console.log(...text); // returns a string
+console.log(spreadText); // returns an array
+
+function Paint(type, use, color, finish) {
+    this.type = type;
+    this.use = use;
+    this.attributes.color = color; 
+    this.attributes.finish = finish;
+    
+    get color() { return this.attributes.color };
+    get finish() { return this.attributes.finish }
+};
+
+let bathroomPaint = {
     type: 'latex',
     use: 'indoor',
     attributes: {
@@ -15,5 +39,6 @@ let wallPaint = {
 };
 
 
-console.log(wallPaint.color);
-console.log(wallPaint.finish);
+console.log(bathroomPaint);
+console.log(bathroomPaint.color);
+console.log(bathroomPaint.finish);
