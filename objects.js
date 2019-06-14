e4001e54b32fbf168162dc5bb4e9886367eddf27
Rob Martin <rob.martin.5758@gmail.com>
2019-06-13 20:10:12 -0400

    spread and object.js changes

--- objects.js
+++ objects.js
@@ -1,4 +1,45 @@
 "use strict"
 const test = Object.freeze({a:1});
-test.a = 2;
+// test.a = 2; // not allowed because of freeze
 console.log(test.a)
+
+function Paint(type, use, color, finish) {
+    this.type = type;
+    this.use = use;
+    function attribute(color, finish) {
+        this.color = color;
+        this.finish = finish;
+    }
+
+    this.attributes = new attribute (color,);
+    this.attributes = new attribute (finish);
+    this.attributes.finish = finish;
+    // get color() { return this.attributes.color };
+    // get finish() { return this.attributes.finish }
+};
+// put methods in prototype only so not all instances have a copy
+ Paint.prototype.color = function () { return this.attributes.color };
+ Paint.prototype.finish = function () { return this.attributes.finish };
+
+let kitchenPaint = new Paint('latex', 'indoor', 'white', 'eggshell');
+console.log('kitchen Paint:' + ' ' + JSON.stringify(kitchenPaint));
+console.log(kitchenPaint); 
+console.log(kitchenPaint.attributes.finish); // eggshell
+console.log(kitchenPaint.color()); // white
+console.log(kitchenPaint.finish()); // eggshell
+// object literal
+let bathroomPaint = {
+    type: 'latex',
+    use: 'indoor',
+    attributes: {
+        color: '#a63f50',
+        finish: 'satin'
+    },
+    get color() { return this.attributes.color },
+    get finish() { return this.attributes.finish }
+};
+console.log('bathroom paint:')
+// don't need to call getter/setter like a method - no ()
+console.log(bathroomPaint);
+console.log(bathroomPaint.color);
+console.log(bathroomPaint.finish);
--- spread.js
+++ spread.js
@@ -10,7 +10,7 @@ console.log([...trees, 'pine']); // returns all 3 trees as array
 // So the individual items become function arguments.
 let text = 'Then there was only one';
 let spreadText = [...text];
-console.log(`typeof spreadText: ${typeof spreadText} isArray: ${Array.isArray(spreadText)}`);
+console.log(`typeof spreadText: ${typeof spreadText} isArray: ${Array.isArray(spreadText)}`); // object true
 // Each individual member of the array was plucked from its location and kept inside the receiver. 
 // The receiver, in this case, is a function. 
 // So the individual items become function arguments.
@@ -20,12 +20,20 @@ console.log(spreadText); // returns an array
 function Paint(type, use, color, finish) {
     this.type = type;
     this.use = use;
-    this.attributes.color = color; 
-    this.attributes.finish = finish;
-    
-    get color() { return this.attributes.color };
-    get finish() { return this.attributes.finish }
+    function attributes(color, finish) {
+        this.color = color;
+        this.finish = finish;
+    }
+
+    this.attributes = new attributes (color,finish);
+    // get color() { return this.attributes.color };
+    // get finish() { return this.attributes.finish }
 };
+
+let kitchenPaint = new Paint('latex', 'indoor', 'white', 'eggshell');
+console.log('kitchen Paint:' + ' ' + JSON.stringify(kitchenPaint));
+console.log(kitchenPaint.attributes.finish)
+
 
 let bathroomPaint = {
     type: 'latex',
@@ -42,3 +50,22 @@ let bathroomPaint = {
 console.log(bathroomPaint);
 console.log(bathroomPaint.color);
 console.log(bathroomPaint.finish);
+
+
+function cimdb(name, review, year) {
+
+    function nestedConstructor(name, review, year) {
+        this.name = name;
+        this.review = review;
+        this.year = year
+    };
+
+    this.name = name;
+    this[name] = new nestedConstructor(name, review, year);
+
+}
+
+
+
+var lionking = new cimdb("The Lion King", "The lion King review ..", 2015);
+console.log(lionking)
