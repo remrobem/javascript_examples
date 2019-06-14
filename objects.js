bb433275596041e94a907ba1e0c9df021e2ea8c2
Rob Martin <rob.martin.5758@gmail.com>
2019-06-14 11:54:08 -0400

    object js changes

--- objects.js
+++ objects.js
@@ -3,30 +3,40 @@ const test = Object.freeze({a:1});
 // test.a = 2; // not allowed because of freeze
 console.log(test.a)
 
-function Paint(type, use, color, finish) {
+function Paint(type='unknown', use='unknown', color, finish) {
     this.type = type;
     this.use = use;
-    function attribute(color, finish) {
+    function Attribute(color, finish) {
         this.color = color;
         this.finish = finish;
-    }
-
-    this.attributes = new attribute (color,);
-    this.attributes = new attribute (finish);
-    this.attributes.finish = finish;
-    // get color() { return this.attributes.color };
-    // get finish() { return this.attributes.finish }
+    };
+    this.attributes = new Attribute (color,finish);
+    if (finish) {
+        this.attributes.finish = finish;
+    };
+    if (color) {
+        this.attributes.color = color;
+    };
 };
+
 // put methods in prototype only so not all instances have a copy
  Paint.prototype.color = function () { return this.attributes.color };
  Paint.prototype.finish = function () { return this.attributes.finish };
 
 let kitchenPaint = new Paint('latex', 'indoor', 'white', 'eggshell');
 console.log('kitchen Paint:' + ' ' + JSON.stringify(kitchenPaint));
-console.log(kitchenPaint); 
-console.log(kitchenPaint.attributes.finish); // eggshell
-console.log(kitchenPaint.color()); // white
-console.log(kitchenPaint.finish()); // eggshell
+console.log(`kitchenPaint.attributes.finish: ${kitchenPaint.attributes.finish}`); // eggshell
+console.log(`kitchenPaint.color(): ${kitchenPaint.color()}`); // white
+console.log(`kitchenPaint.finish(): ${kitchenPaint.finish()}`); // eggshell
+
+let type, use, color;
+let finish = 'gloss';
+let emptyPaint = new Paint(type, use, color, finish);
+console.log('empty Paint:' + ' ' + JSON.stringify(emptyPaint));
+console.log(`emptyPaint.attributes.finish: ${emptyPaint.attributes.finish}`); // eggshell
+console.log(`emptyPaint.color(): ${emptyPaint.color()}`); // white
+console.log(`emptyPaint.finish(): ${emptyPaint.finish()}`); // eggshell
+
 // object literal
 let bathroomPaint = {
     type: 'latex',
