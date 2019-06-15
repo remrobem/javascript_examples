89fae9d4c3989f603ad17b16ddfd4376ff77de13
Rob Martin <rob.martin.5758@gmail.com>
2019-06-15 12:02:00 -0400

    object.js changes

--- objects.js
+++ objects.js
@@ -1,39 +1,92 @@
 "use strict"
-const test = Object.freeze({a:1});
+const test = Object.freeze({ a: 1 });
 // test.a = 2; // not allowed because of freeze
 console.log(test.a)
 
 // standard constructor function
-// allows for nested object
-function Paint(type = 'unknown', use = 'unknown', color = 'N/A', finish = 'N/A') {
+// this instance allows for nested object
+function Paint(type = 'unknown', use = 'unknown', color = 'N/A', finish = 'N/A', price = 'N/A') {
     this.type = type;
     this.use = use;
-    this.attributes = new Attribute (color,finish);
-    this.attributes.style = 'wall';
-    
+    this.price = price;
+    this.priceChanges = [];
+    this.attributes = new Attribute(color, finish);
+    this.attributes.style = 'wall'; // not passed in argument list
+
+    // defines the nested object. used above to create attributes
     function Attribute(color, finish) {
         this.color = color;
         this.finish = finish;
     };
+
+    // define getter for constructor
+    // these execute implicitly when price is referenced or updated
+    // Object.defineProperty(this, "price", {
+    //     get() {
+    //         return typeof price == 'number' ? `$${price}` : price;
+    //     },
+    //     set(newPrice) { 
+    //         // store price change details
+    //         let priceChange = ((newPrice * 100) - (price * 100)) / 100;
+    //         if (priceChange != 0) {
+    //             this.priceChanges.push(
+    //                 {
+    //                     'originalPrice' : price,
+    //                     'newPrice' : newPrice,
+    //                     'change' : priceChange
+    //                 }
+    //             )
+    //         }
+    //         console.log(`Price Change: ${priceChange}`) 
+    //     }
+    // });
 };
 // put methods in prototype only so not all instances have a copy
- Paint.prototype.getColor = function () { return this.attributes.color };
- Paint.prototype.getFinish = function () { return this.attributes.finish };
+Paint.prototype.getColor = function () { return this.attributes.color };
+Paint.prototype.getFinish = function () { return this.attributes.finish };
 
-let kitchenPaint = new Paint('latex', 'indoor', 'white', 'eggshell');
+ // define getter for constructor
+// these execute implicitly when price is referenced or updated
+Object.defineProperty(Paint.prototype, "this.price", {
+    get() {
+        return typeof this.price == 'number' ? `$${this.price}` : this.price;
+    },
+    set(newPrice) { 
+        // store price change details
+        let priceChange = ((newPrice * 100) - (this.price * 100)) / 100;
+        if (priceChange != 0) {
+            this.priceChanges.push(
+                {
+                    'originalPrice' : this.price,
+                    'newPrice' : newPrice,
+                    'change' : priceChange
+                }
+            )
+        }
+        console.log(`Price Change: ${priceChange}`) 
+    }
+});
+
+
+let kitchenPaint = new Paint('latex', 'indoor', 'white', 'eggshell', 27.50);
+kitchenPaint.price = 12.14;
+kitchenPaint.price = 41.23;
+kitchenPaint.price = 27.90;
 console.log(`kitchen Paint: ${JSON.stringify(kitchenPaint)}`);
 
 // null and undefined parameters
-let type, use, color, finish;
+let type, use, color, finish, price;
 finish = null;
 color = undefined;
+price = 0;
 let emptyPaint = new Paint(type, use, color, finish);
 console.log(`empty Paint: ${JSON.stringify(emptyPaint)}`);
-console.log(`emptyPaint.getColor(): ${emptyPaint.getColor()}`); 
-console.log(`emptyPaint.getFinish(): ${emptyPaint.getFinish()}`); 
+console.log(`emptyPaint.getColor(): ${emptyPaint.getColor()}`);
+console.log(`emptyPaint.getFinish(): ${emptyPaint.getFinish()}`);
 
 
 // object literal
+// allows use of getter/setter directly
 let bathroomPaint = {
     type: 'latex',
     use: 'indoor',
